a7b21e38f3c90bd56c54707034f66ed90e965283
Rodrigo Soares <soaresjw27@gmail.com>
2025-04-16 15:58:30 -0300

    Projeto Spring boot JPA

---
+++ Atv03_PI_Biblioteca/src/main/java/com/atv03/PI/uc15/Biblioteca/BibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.atv03.PI.uc15.Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
---
+++ Atv03_PI_Biblioteca/src/test/java/com/atv03/PI/uc15/Biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atv03.PI.uc15.Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
